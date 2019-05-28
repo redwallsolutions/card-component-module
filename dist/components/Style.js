cb01742fa277ce65722f26906a541a491ed9d394
Andrew Ribeiro <andtankian@live.com>
2019-05-28 10:43:42 -0300

    Added, bug fixes to distribution.

--- dist/components/Style.js
+++ dist/components/Style.js
@@ -11,7 +11,7 @@ function _templateObject8() {
 }
 
 function _templateObject7() {
-  var data = _taggedTemplateLiteral(["\n  position: relative;\n  transition: all 0.2s ease;\n  border-radius: 5px;\n  background: ", ";\n  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.14), 0 3px 10px -2px rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.12);\n  padding: 1em;\n  &:hover ", " {\n    transform: translate(0, -1%);\n    box-shadow: 0 16px 38px -12px rgba(0, 0, 0, .56), 0 4px 25px 0 rgba(0, 0, 0, .12), 0 8px 10px -5px rgba(0, 0, 0, .2);\n  }\n"]);
+  var data = _taggedTemplateLiteral(["\n  position: relative;\n  transition: all 0.2s ease;\n  border-radius: 5px;\n  background: ", ";\n  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.14), 0 3px 10px -2px rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.12);\n  padding: 1em;\n  &:hover ", " {\n    transform: translate(0, -2%);\n    box-shadow: 0 16px 38px -12px rgba(0, 0, 0, .56), 0 4px 25px 0 rgba(0, 0, 0, .12), 0 8px 10px -5px rgba(0, 0, 0, .2);\n  }\n"]);
 
   _templateObject7 = function _templateObject7() {
     return data;
@@ -21,7 +21,7 @@ function _templateObject7() {
 }
 
 function _templateObject6() {
-  var data = _taggedTemplateLiteral(["\n  width: 94%;\n  top: -15%;\n  left: 3%;\n  border-radius: 4px;\n  transition: all 0.2s ease;\n  color: ", ";\n  background: ", ";\n  box-shadow: 0 16px 38px -12px rgba(0, 0, 0, .12), 0 4px 25px 0 rgba(0, 0, 0, .2), 0 8px 10px -5px rgba(0, 0, 0, .08);\n  position: absolute;\n"]);
+  var data = _taggedTemplateLiteral(["\n  position: absolute;\n  width: 94%;\n  top: -20px;\n  left: 3%;\n  border-radius: 4px;\n  transition: all 0.2s ease;\n  color: ", ";\n  background: ", ";\n  box-shadow: 0 16px 38px -12px rgba(0, 0, 0, .12), 0 4px 25px 0 rgba(0, 0, 0, .2), 0 8px 10px -5px rgba(0, 0, 0, .08);\n"]);
 
   _templateObject6 = function _templateObject6() {
     return data;
